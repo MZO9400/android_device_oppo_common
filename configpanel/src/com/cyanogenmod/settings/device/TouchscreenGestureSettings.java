273924404e7ae8f4747f97ee3fb839e3afdd61d6
Meninblack007 <sanyam.53jain@gmail.com>
2016-08-16 19:31:18 +0530

    Revert "Use ScreenType util from cmsdk"
    This reverts commit 0960663eba5d8b61096a57bf117b441a4e016eed.

--- configpanel/src/com/cyanogenmod/settings/device/TouchscreenGestureSettings.java
+++ configpanel/src/com/cyanogenmod/settings/device/TouchscreenGestureSettings.java
@@ -18,41 +18,49 @@ package com.cyanogenmod.settings.device;
 
 import com.android.internal.util.cm.ScreenType;
 
-import com.cyanogenmod.settings.device.utils.Constants;
 import com.cyanogenmod.settings.device.utils.NodePreferenceActivity;
 
 import android.os.Bundle;
-import android.os.IBinder;
-import android.os.ServiceManager;
+import android.provider.Settings;
 import android.preference.Preference;
-import android.service.gesture.IGestureService;
+import android.preference.SwitchPreference;
 
-public class GesturePadSettings extends NodePreferenceActivity {
+public class TouchscreenGestureSettings extends NodePreferenceActivity {
+    private static final String KEY_HAPTIC_FEEDBACK = "touchscreen_gesture_haptic_feedback";
+
+    private SwitchPreference mHapticFeedback;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        addPreferencesFromResource(R.xml.gesture_panel);
+        addPreferencesFromResource(R.xml.touchscreen_panel);
 
-        Preference p = findPreference(Constants.TOUCHPAD_DOUBLETAP_KEY);
-        p.setOnPreferenceChangeListener(this);
-        p = findPreference(Constants.TOUCHPAD_LONGPRESS_KEY);
-        p.setOnPreferenceChangeListener(this);
+        mHapticFeedback = (SwitchPreference) findPreference(KEY_HAPTIC_FEEDBACK);
+        mHapticFeedback.setOnPreferenceChangeListener(this);
     }
 
     @Override
     public boolean onPreferenceChange(Preference preference, Object newValue) {
-        if (preference.getKey().equals(Constants.TOUCHPAD_DOUBLETAP_KEY)) {
-            IBinder b = ServiceManager.getService("gesture");
-            IGestureService sInstance = IGestureService.Stub.asInterface(b);
-            Startup.toggleDoubleTap(this, sInstance, (Boolean) newValue);
-            return true;
-        } else if (preference.getKey().equals(Constants.TOUCHPAD_LONGPRESS_KEY)) {
-            IBinder b = ServiceManager.getService("gesture");
-            IGestureService sInstance = IGestureService.Stub.asInterface(b);
-            Startup.toggleLongPress(this, sInstance, (Boolean) newValue);
+        final String key = preference.getKey();
+        if (KEY_HAPTIC_FEEDBACK.equals(key)) {
+            final boolean value = (Boolean) newValue;
+            Settings.System.putInt(getContentResolver(), KEY_HAPTIC_FEEDBACK, value ? 1 : 0);
             return true;
         }
+
         return super.onPreferenceChange(preference, newValue);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        // If running on a phone, remove padding around the listview
+        if (!ScreenType.isTablet(this)) {
+            getListView().setPadding(0, 0, 0, 0);
+        }
+
+        mHapticFeedback.setChecked(
+                Settings.System.getInt(getContentResolver(), KEY_HAPTIC_FEEDBACK, 1) != 0);
     }
 }
